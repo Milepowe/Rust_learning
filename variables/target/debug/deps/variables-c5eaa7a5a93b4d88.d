db6c422870e7eefe7829bb43ca9abe737889c918
Eleve CSMH <Eleve CSMH@MARISTE.LOCAL>
2024-02-23 11:09:31 +0000

    modified variables

---
+++ variables/target/debug/deps/variables-c5eaa7a5a93b4d88.d
@@ -0,0 +1,5 @@
+C:\Users\Eleve CSMH\Documents\cooking\rust\Rust_learning\variables\target\debug\deps\libvariables-c5eaa7a5a93b4d88.rmeta: src\main.rs
+
+C:\Users\Eleve CSMH\Documents\cooking\rust\Rust_learning\variables\target\debug\deps\variables-c5eaa7a5a93b4d88.d: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/deps/variables-cce405208e33a35a.d
@@ -0,0 +1,5 @@
+C:\Users\Eleve CSMH\Documents\cooking\rust\Rust_learning\variables\target\debug\deps\libvariables-cce405208e33a35a.rmeta: src\main.rs
+
+C:\Users\Eleve CSMH\Documents\cooking\rust\Rust_learning\variables\target\debug\deps\variables-cce405208e33a35a.d: src\main.rs
+
+src\main.rs:
