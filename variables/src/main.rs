5814b55de28ce672b4ce15a83d77ce1accf47736
Milepowe <nilsbitho717@gmail.com>
2024-02-27 22:39:46 +0000

    numeric operator

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -21,4 +21,20 @@ fn main() {
     let x = 2.0; // f64
 
     let y: f32 = 3.0; // f32
+
+     // addition
+     let sum = 5 + 10;
+
+     // subtraction
+     let difference = 95.5 - 4.3;
+ 
+     // multiplication
+     let product = 4 * 30;
+ 
+     // division
+     let quotient = 56.7 / 32.2;
+     let truncated = -5 / 3; // Results in -1
+ 
+     // remainder
+     let remainder = 43 % 5;
 }
