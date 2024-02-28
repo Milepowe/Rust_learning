7a35a495befd9e28dc943f1c09f4da07d9368342
Milepowe <nilsbitho717@gmail.com>
2024-02-28 14:43:16 +0000

    added tuples and array type

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,4 @@
+use std::io;
 fn main() {
     let x = 5;
 
@@ -51,4 +52,43 @@ fn main() {
     let (x, y, z) = tup;
 
     println!("The value of y is: {y}");
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+
+    let a = [1, 2, 3, 4, 5];
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+    //array type is i32
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
+    //array of 5 elements that are set to the value 3
+    let a = [3; 5];
+
+    let a = [1, 2, 3, 4, 5];
+
+    let first = a[0];
+    let second = a[1];
+
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+    // if we try to input a value past the end of the array we will get an error
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
