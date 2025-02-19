c8a231e51f06718d1c800634b867a81296841fee
Milepower <90985551+Milepowe@users.noreply.github.com>
2025-02-19 10:16:04 +0000

    Delete guessing_game directory

--- guessing_game/target/debug/deps/guessing_game-19ee4ca1e4ba4b61.d
+++
@@ -1,5 +0,0 @@
-/home/mile/programming/rust/guessing_game/target/debug/deps/guessing_game-19ee4ca1e4ba4b61: src/main.rs
-
-/home/mile/programming/rust/guessing_game/target/debug/deps/guessing_game-19ee4ca1e4ba4b61.d: src/main.rs
-
-src/main.rs:
