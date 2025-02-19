a1f09e6fa4b249c12d9e879710f415acdbd6facb
Milepower <90985551+Milepowe@users.noreply.github.com>
2025-02-19 10:16:16 +0000

    Delete variables directory

--- variables/target/debug/deps/variables-c5eaa7a5a93b4d88.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Eleve CSMH\Documents\cooking\rust\Rust_learning\variables\target\debug\deps\libvariables-c5eaa7a5a93b4d88.rmeta: src\main.rs
-
-C:\Users\Eleve CSMH\Documents\cooking\rust\Rust_learning\variables\target\debug\deps\variables-c5eaa7a5a93b4d88.d: src\main.rs
-
-src\main.rs:
--- variables/target/debug/deps/variables-cce405208e33a35a.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Eleve CSMH\Documents\cooking\rust\Rust_learning\variables\target\debug\deps\libvariables-cce405208e33a35a.rmeta: src\main.rs
-
-C:\Users\Eleve CSMH\Documents\cooking\rust\Rust_learning\variables\target\debug\deps\variables-cce405208e33a35a.d: src\main.rs
-
-src\main.rs:
